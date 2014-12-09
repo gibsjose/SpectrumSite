1d302298376cb120de607573495be21ab004c078
Joe Gibson <gibsjose@mail.gvsu.edu>
2014-12-09 12:21:43 +0100

    Debug

--- get_steering_files.php
+++ get_steering_files.php
@@ -2,8 +2,8 @@
 <!-- Scan the 'Steering' directory and create options for each entry -->
 <?php
     $output = shell_exec('find Steering/ -name *.txt');
-    $steering_array = preg_split("/\r\n|\n|\r|\s/", $string);
-
+    $steering_array = preg_split("/\r\n|\n|\r| /", $output);
+    //$steering_array = explode(" ", $)
     echo "$output";
     var_dump($steering_array);
     echo "<option>None</option>\n";
