64b4fe579714342e1025fdb4d9a9c12b7ddbda1c
Joe Gibson <gibsjose@mail.gvsu.edu>
2014-12-11 15:11:36 +0100

    Debug

--- load_plot.php
+++ load_plot.php
@@ -57,17 +57,38 @@
         $ratio_styles = explode(":", $ratio_style);
         $ratios = explode(":", $ratio);
 
+        //Create the zip for the data files
+        $d_zip = new ZipArchive();
+        $data_zip_filename = "tmp/data.zip";
+        $d_zip->open($data_zip_filename, ZipArchive::CREATE)
+
         //Get the data, grid, and pdf directories from the selected files
         foreach($data_steerings as $file) {
-            //echo "<h2>$file</h2>";
             $data_directory = $data_directory . dirname($file) . ",";
             $data_file = $data_file . basename($file) . ",";
+
+            $actual_data_file = basename($data_file, '.txt') . "_data.txt";
+            $file_to_add = $data_directory . $actual_data_file;
+            $d_zip->addFile($file_to_add);
         }
+
+        $d_zip->close();
+
+        //Create the zip for the grid files
+        $g_zip = new ZipArchive();
+        $grids_zip_filename = "tmp/grids.zip";
+        $g_zip->open($grids_zip_filename, ZipArchive::CREATE)
 
         foreach($grid_steerings as $file) {
             $grid_directory = $grid_directory . dirname($file) . ",";
             $grid_file = $grid_file . basename($file) . ",";
+
+            $actual_grid_file = basename($grid_file, '.txt') . ".root";
+            $file_to_add = $grid_directory . $actual_grid_file;
+            $g_zip->addFile($file_to_add);
         }
+
+        $g_zip->close();
 
         foreach($pdf_steerings as $file) {
             $pdf_directory = $pdf_directory . dirname($file) . ",";
@@ -150,10 +171,10 @@
             <a href=<?php print("\"$plot\"")?> target="_newtab"><button width="90%" class="button-plot pure-button"><i class="fa fa-chevron-circle-down"></i> Plot</button></a>
         </div>
         <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
-            <a href=<?php print("\"#\"")?> target="_newtab"><button width="90%" class="button-data pure-button"><i class="fa fa-chevron-circle-down"></i> Data</button></a>
+            <a href=<?php print("\"tmp/data.zip\"")?> target="_newtab"><button width="90%" class="button-data pure-button"><i class="fa fa-chevron-circle-down"></i> Data</button></a>
         </div>
         <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
-            <a href=<?php print("\"#\"")?> target="_newtab"><button width="90%" class="button-grid pure-button"><i class="fa fa-chevron-circle-down"></i> Grid</button></a>
+            <a href=<?php print("\"tmp/grids.zip\"")?> target="_newtab"><button width="90%" class="button-grid pure-button"><i class="fa fa-chevron-circle-down"></i> Grid</button></a>
         </div>
         <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
             <a href=<?php print("\"$steering\"")?> target="_newtab"><button width="90%" class="button-steering pure-button"><i class="fa fa-chevron-circle-down"></i> Steering</button></a>
