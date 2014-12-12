e3f1ed9358f935f23efaf61768f12f570f955a59
Joe Gibson <gibsjose@mail.gvsu.edu>
2014-12-12 11:56:04 +0100

    Debug

--- get_steering_files.php
+++ get_steering_files.php
@@ -1,5 +1,5 @@
 <!-- Scan the 'Steering' directory and create options for each entry -->
-<!-- <?php
+<?php
     $output = shell_exec('find Steering/ -name *.txt');
     $steering_array = preg_split("/\r\n|\n|\r| /", $output);
 
@@ -9,5 +9,23 @@
         $full = $file;
         $stripped = basename($file, '.txt');
         echo "<option value='$full'>$stripped</option>\n";
+    }
+?>
+
+<!-- <?php
+    function startsWith($haystack, $needle) {
+        return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
+    }
+?>
+
+<?php
+    $s_config = 'config/steering.config';
+    $config_contents = file_get_contents($s_config);
+    $lines = explode('\n', $config_contents);
+
+    foreach($lines as $line) {
+        if(startsWith($line, "#")) {
+
+        }
     }
 ?> -->
