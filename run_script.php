81dbad48e996d774500b141f3990290c70165f00
Joe Gibson <gibsjose@mail.gvsu.edu>
2014-12-15 11:33:59 +0100

    Debug

--- run_script.php
+++ run_script.php
@@ -3,11 +3,11 @@
     $script = $_POST['script'];
     $flags = $_POST['flags'];
 
-    print("<h2>$script</h2>");
-    print("<h2>$flags</h2>");
+    // print("<h2>$script</h2>");
+    // print("<h2>$flags</h2>");
 
-    $test = $script . $flags;
-    print("<h2>$test</h2>");
+    $test = $script . " " . $flags;
+    // print("<h2>$test</h2>");
 
     $output = shell_exec($script . $flags);
     print("<h2>$output</h2>");
