044f43945ff7b48002c9cac12dfff361eac9d0f3
Joe Gibson <gibsjose@mail.gvsu.edu>
2014-12-15 15:22:09 +0100

    Debug

--- plot.php
+++ plot.php
@@ -346,7 +346,7 @@
                     $('#test-container').load('run_script.php', data);
                     $('#grid_steering').load('run_script.php', data);
                 } else {
-                    $('#test-container').append($('').load('run_script.php', data));
+                    $('#test-container').append($('<h1>').load('run_script.php', data));
                     $('#grid_steering').append($('<div>').load('run_script.php', data));
                 }
 
