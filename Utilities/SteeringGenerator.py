9325eef9cc5443ddf19637075d44179a26083d45
Joe Gibson <gibsjose@mail.gvsu.edu>
2014-12-17 11:34:47 +0100

    Not requiring color/style specification

--- Utilities/SteeringGenerator.py
+++ Utilities/SteeringGenerator.py
@@ -11,8 +11,7 @@ defaults = {}
 # Checks whether the key exists in the input dictionary
 def Write(_key, _file):
     if _key in d:
-        sys.err.write(d[_key])
-        if not d[_key]:
+        if not strip(d[_key]):
             _file.write("; ")
 
         _file.write(_key + ' = ' + d[_key])
